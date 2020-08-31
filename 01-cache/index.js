1895d9978f8d2ac9a2e0d5f6365ad141b1b6922c
饶家俊 <raojiajun@bimwinner.com>
2020-08-31 21:48:48 +0800

    test

--- 01-cache/index.js
+++ 01-cache/index.js
@@ -3,27 +3,25 @@ const app = express()
 const port = 3000
 const path = require('path')
 
+app.set('etag', false); // turn off
+
+
 app.get('/', (req, res) => {
 	console.log('request /');
 	res.set({
-		'Content-type': 'text/html;',
-		// 'Cache-Control': 'max-age=10'
+		'Cache-Control': 'max-age=20'
 	})
 	res.sendFile(path.join(__dirname + '/index.html'));
 })
 
-app.get('/test', (req, res) => {
-	console.log('request /test');
-
+app.get('/index.css', (req, res) => {
+	console.log('request css');
 	res.set({
-		'Cache-Control': 'max-age=30'
+		'Cache-Control': 'max-age=20'
 	})
-	
-	res.send({
-		test: 1
-	})
-	res.sendFile(path.join(__dirname + '/index.html'));
+	res.sendFile(path.join(__dirname + '/index.css'));
 })
+
 
 app.listen(port, () => {
 	console.log(`Example app listening at http://localhost:${port}`)
